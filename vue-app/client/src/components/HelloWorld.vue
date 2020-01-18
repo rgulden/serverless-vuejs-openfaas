f9d15c174a811ed47172a730d44bebfdf7dc3f4a
guldentech <rgulden@live.com>
2020-01-18 17:53:58 -0600

    Fixed pictures to be in container

--- vue-app/client/src/components/HelloWorld.vue
+++ vue-app/client/src/components/HelloWorld.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <b-carousel
-      id="carousel-fade"
-      style="text-shadow: 0px 0px 2px #000"
-      fade
-      indicators
-      img-width="1024"
-      img-height="480"
-    >
-      <b-carousel-slide
-        caption="First slide"
-        img-src="https://picsum.photos/1024/480/?image=10"
-      ></b-carousel-slide>
-      <b-carousel-slide
-        caption="Second Slide"
-        img-src="https://picsum.photos/1024/480/?image=12"
-      ></b-carousel-slide>
-      <b-carousel-slide
-        caption="Third Slide"
-        img-src="https://picsum.photos/1024/480/?image=22"
-      ></b-carousel-slide>
-    </b-carousel>
+    <b-container>
+      <b-carousel
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade
+        indicators
+        img-width="1024"
+        img-height="480"
+      >
+        <b-carousel-slide
+          caption="First slide"
+          img-src="https://picsum.photos/1024/480/?image=10"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Second Slide"
+          img-src="https://picsum.photos/1024/480/?image=12"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Third Slide"
+          img-src="https://picsum.photos/1024/480/?image=22"
+        ></b-carousel-slide>
+      </b-carousel>
+    </b-container>
   </div>
 </template>
 
